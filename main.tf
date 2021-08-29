e387f680761fa7c7671f5eb57409baa49a9af476
alexpitronot <87993785+alexpitronot@users.noreply.github.com>
2021-08-29 17:25:48 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,15 +9,3 @@ resource "github_repository" "demo-1" {
   license_template = "mit"
   topics           = ["example", "public", "infrastructure-as-code", "operations", "terraform", "github"]
 }
-
-resource "github_repository" "demo-2" {
-  name             = "demo-2"
-  description      = "A demo GitHub repository created by hand and imported into Terraform"
-  private          = false  
-  has_projects     = false
-  has_wiki         = false
-  has_downloads    = false
-  has_issues       = true
-  #license_template = "mit"
-  topics           = ["example", "public", "infrastructure-as-code", "operations", "terraform", "github"]
-}
